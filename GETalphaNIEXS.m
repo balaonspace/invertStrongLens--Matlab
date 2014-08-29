653aea62ff4e0233cab425920871b154b9d2d560
balaonspace <balaonspace@gmail.com>
2014-08-29 10:01:19 +0200

    Create GETalphaNIEXS.m
    This function returns the deflection angle for the given lens parameters and lens plane grid.
    The lens plane grid is either the center of the pixels or the edge of the pixels.

---
+++ GETalphaNIEXS.m
@@ -0,0 +1,24 @@
+function [Xr,alpha,x1,x2] = GETalphaNIEXS(lensParam,X)
+% % % % calculate the deflection angle for the nonsingular isothermal
+% ellipsoid lens model with external shear
+
+% % % lens parameters
+einRad = lensParam(1);
+f = lensParam(2);
+fp = sqrt(1-f^2);
+bc = lensParam(3);
+t = lensParam(4);
+xshear = complex(lensParam(5),lensParam(6));
+
+% % % % rotate the co-ord according to lens orientation
+Xr = X*exp(-1i*t);
+x1 = real(Xr);
+x2 = imag(Xr);
+
+b = complex(x1,f*x2);
+bsq = abs(b).^2;
+bsqx = complex(x1,f^2*x2);
+
+alpha = einRad*(sqrt(f)/fp)*( atanh( fp * sqrt(bsq+bc^2) ./ bsqx ) - atanh( fp*bc./(f*Xr)) );
+alpha = conj(alpha);
+alpha = alpha*exp(1i*lensParam(4)) - xshear*conj(X);
